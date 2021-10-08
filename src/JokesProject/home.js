87ad65743eceef8d4bf0c0e7d89e9c87fdb36315
rehanali7996 <70329407+rehanali7996@users.noreply.github.com>
2021-10-08 17:27:28 +0530

    react bootstrap footer added
    footer element added in home.js file

--- src/JokesProject/home.js
+++ src/JokesProject/home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { ModalFooter } from 'react-bootstrap';
 import App from './Modal';
 
 export default function QuotesHome(){
     return(
         <div className="container-fluid" style={{display:"grid",placeItems:"center", height:"100vh",backgroundImage:"linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url(/images/background.jpg)",backgroundRepeat:"no-repeat",backgroundPosition:"center",backgroundSize:"cover"}}>
             <App/>
-
+            <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                <ModalFooter><p style="font-family: 'Spartan';font-size: 12px;">Created with <span><i class="fas fa-heart fa-lg"></i></span> Rehan </p></ModalFooter>
+            </div>
         </div>
     )    
 }
