fb033a997fc8ec212ce3892822416d66b42418fe
rehanali7996 <70329407+rehanali7996@users.noreply.github.com>
2021-10-08 18:55:34 +0530

    edited footer
    react icons in footer

--- src/JokesProject/home.js
+++ src/JokesProject/home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { ModalFooter } from 'react-bootstrap';
 import App from './Modal';
+import { FaHandHoldingHeart } from "react-icons/fa";
 
 export default function QuotesHome(){
     return(
         <div className="container-fluid" style={{display:"grid",placeItems:"center", height:"100vh",backgroundImage:"linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url(/images/background.jpg)",backgroundRepeat:"no-repeat",backgroundPosition:"center",backgroundSize:"cover"}}>
             <App/>
-            <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                <ModalFooter><p style="font-family: 'Spartan';font-size: 12px;">Created with <span><i class="fas fa-heart fa-lg"></i></span> Rehan </p></ModalFooter>
+            <div className="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);border:1px solid #FFCC00;border-radius:10px">
+                <p style="font-family: 'Spartan';font-size: 12px;">Created with <FaHandHoldingHeart/> Rehan </p>
             </div>
         </div>
     )    
